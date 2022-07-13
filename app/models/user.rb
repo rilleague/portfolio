0544c0987357cce1c391f15daddafb03654bf898
rilleague <aarima2626@gmail.com>
2022-07-13 23:47:58 +0900

    Userモデル内のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    # 半角英数字6文字以上でないと保存出来ない
+    PASSWORD_REGEX = /\A[a-z\d]{6,}+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
+    # 主にはドットとアットマークを含まないと保存できない,同じメールアドレスは保存出来ない
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+    # 同じニックネームは保存出来ない
+    validates :nickname, uniqueness: true
+  end
 end
