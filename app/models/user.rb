1575d16f24d108c5f907ff956cc95a81d7589d67
rilleague <aarima2626@gmail.com>
2022-07-14 16:12:34 +0900

    デフォルトのバリデーションと重複した記述をしていた為、一部変更及び削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    # 半角英数字6文字以上でないと保存出来ない
-    PASSWORD_REGEX = /\A[a-z\d]{6,}+\z/i.freeze
+    # 半角英数字でないと保存出来ない
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX
-    # 主にはドットとアットマークを含まないと保存できない,同じメールアドレスは保存出来ない
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
     # 同じニックネームは保存出来ない
     validates :nickname, uniqueness: true
   end
