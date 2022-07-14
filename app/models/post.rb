382d6d206f2a6f7d3ed7070810f61d6f87ac2b05
rilleague <aarima2626@gmail.com>
2022-07-14 19:02:36 +0900

    投稿に対して、其々の項目を紐付ける為、モジュールを導入しアソシエーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploaders :images, ImageUploader
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :part
+  belongs_to :skin
 end
