30aeb00d82fcbd7f85e0edee631aeca7d2e670ac
rilleague <aarima2626@gmail.com>
2022-07-14 19:42:22 +0900

    newアクション,createアクション,ストロングパラメーターを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,19 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :category_id, :part_id, :skin_id, :detail, { images: [] }).merge(user_id: current_user.id)
   end
 end
