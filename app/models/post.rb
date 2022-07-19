bfb48d0ef2ad12c0c3d728d8b80df0b1bc2fe974
rilleague <aarima2626@gmail.com>
2022-07-19 19:17:14 +0900

    中間テーブルのアソシエーションの記述を変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   mount_uploaders :images, ImageUploader
   serialize :images, JSON
-  has_many :tags, through: :post_tags
   has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :part
