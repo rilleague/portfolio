2d0462ebca2185e8aa2ee59059b0d1ffce3c0b26
rilleague <aarima2626@gmail.com>
2022-07-20 20:44:45 +0900

    投稿詳細画面のビューを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new]
+  before_action :authenticate_user!, only: [:index, :new, :show]
 
   def index
     @category_id = params[:category_id]
