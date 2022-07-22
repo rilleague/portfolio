084fed043fa8df68bb3f483a0fd8d17287253bf2
rilleague <aarima2626@gmail.com>
2022-07-22 21:34:17 +0900

    編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,13 +31,31 @@ class PostsController < ApplicationController
   end
 
   def edit
+    # 各カラムの中に、paramsで受け取った投稿の中身を入れる
+    @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:tagname).join(",")
+    @form = PostForm.new(title: @post.title, category_id: @post.category_id, part_id: @post.part_id, skin_id: @post.skin_id, detail: @post.detail, images: @post.images )
   end
 
   def update
+    @post = Post.find(params[:id])
+    @form = PostForm.new(update_params)
+    tag_list = params[:post_form][:tagname].split(",")
+    if @form.valid?
+      #下記のupadateは、post_form.rbにて定義しているupdateメソッドである。
+      @form.update(tag_list)
+      redirect_to post_path(post.id)
+    else
+      render :edit
+    end
   end
 
   private
   def post_form_params
     params.require(:post_form).permit(:title, :category_id, :part_id, :skin_id, :detail, :tagname, { images: [] }).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:post_form).permit(:title, :category_id, :part_id, :skin_id, :detail, :tagname, { images: [] }).merge(user_id: current_user.id, post_id: params[:id])
   end
 end
