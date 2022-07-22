ef59c78928e89fadf8fbe8d4f1e181f67ff71e68
rilleague <aarima2626@gmail.com>
2022-07-22 23:04:17 +0900

    コメント保存機能//表示機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @comments = @post.comments.includes(:user)
+      render "posts/#{@post.id}"
+    end
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(post_id: params[:post_id], user_id: current_user.id)
   end
 end
