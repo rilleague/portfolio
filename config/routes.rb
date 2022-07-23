0fdda1c2bc2b245ccff3dc42c08789f74970dd1a
rilleague <aarima2626@gmail.com>
2022-07-23 21:56:04 +0900

    commentsコントローラーのdestroyアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
   resources :posts, only: [:index, :new, :create, :show] do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
 end
