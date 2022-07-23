172a719bd647c40927093773d3f7365e7c1377cb
rilleague <aarima2626@gmail.com>
2022-07-23 21:56:35 +0900

    Commentに関するモデル内の記述訂正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   mount_uploaders :images, ImageUploader
   serialize :images, JSON
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   extend ActiveHash::Associations::ActiveRecordExtensions
