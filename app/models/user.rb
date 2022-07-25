c2e99d3431704ee9141db21115dd65ead537b09b
rilleague <aarima2626@gmail.com>
2022-07-25 19:27:15 +0900

    Post/Userモデルとのアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_posts, through: :favorites, source: :post
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
