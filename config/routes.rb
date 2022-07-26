b4b2aa939ee82743227edadf440be634d1c5f509
rilleague <aarima2626@gmail.com>
2022-07-26 12:22:16 +0900

    usersコントローラーのshowアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'favorites/destroy'
   devise_for :users
   root to: 'pages#index'
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
