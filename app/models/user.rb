493ca5d3903f4370e0cb02d3f35260f43f391c41
rilleague <aarima2626@gmail.com>
2022-07-30 10:47:48 +0900

    userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   extend CarrierWave::Mount
-  belongs_to :age_id
+  belongs_to_active_hash :age
   mount_uploader :avatar, AvatarUploader
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -14,9 +14,9 @@ class User < ApplicationRecord
   with_options presence: true do
     # 半角英数字でないと保存出来ない
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX
+    validates_format_of :password, with: PASSWORD_REGEX, on: :create
     # 同じニックネームは保存出来ない
-    validates :nickname, uniqueness: true
+    validates :nickname, uniqueness: true, length: { maximum: 6 }
   end
   validates :age_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :introduction, length: { maximum: 300 }
