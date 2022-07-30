a7a539729a68d0bd6989b9568185756d362d0fc2
rilleague <aarima2626@gmail.com>
2022-07-30 10:47:37 +0900

    ユーザー情報編集　viewの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'favorites/create'
   get 'favorites/destroy'
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   root to: 'pages#index'
   resources :users, only: [:show, :edit, :update]
   resources :posts do
