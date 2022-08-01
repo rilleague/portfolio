c00332d6a53871a6a05d0acd0b56da562ceed425
rilleague <aarima2626@gmail.com>
2022-08-01 23:48:55 +0900

    マイグレーションファイルの訂正

--- db/migrate/20220730031503_create_relationships.rb
+++ db/migrate/20220730031503_create_relationships.rb
@@ -2,9 +2,9 @@ class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
       t.references :follower,  foreign_key: { to_table: :users }
-      t.references :following, foreign_key: { to_table: :users }
+      t.references :followed, foreign_key: { to_table: :users }
       t.timestamps
-      t.index [:follower_id, :following_id], unique: true
+      t.index [:follower_id, :followed_id], unique: true
     end
   end
 end
