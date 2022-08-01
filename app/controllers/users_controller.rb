6f7c9f0780501d1636bd17f0604c6b6568678476
rilleague <aarima2626@gmail.com>
2022-08-01 12:20:06 +0900

    usersコントローラー内に、フォロー・フォロワー一覧表示の為のメソッドを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
 
   def show
     @posts = @user.posts
@@ -21,6 +21,20 @@ class UsersController < ApplicationController
       flash.now['danger'] = "プロフィールを更新出来ませんでした"
       render :edit
     end
+  end
+
+  # 自分がフォローしているユーザーを表示するメソッドを定義
+  def followings
+    # @user = User.find(params[:id])
+    @users = @user.followings
+    render "show_follower"
+  end
+
+  # 自分をフォローしているユーザーを表示するメソッドを定義
+  def followers
+    # @user = User.find(params[:id])
+    @users = @user.followers
+    render "show_follower"
   end
 
   private
