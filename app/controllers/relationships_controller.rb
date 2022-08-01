f2047e2ed8b5413e18f0621de86e5c33099f46ee
rilleague <aarima2626@gmail.com>
2022-08-01 23:47:35 +0900

    relationshipsコントローラーの修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   # フォローする
   def create
-    @user = User.find(params[:following_id])
-    current_user.follow(@user)
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
 
   # フォロー解除する
   def destroy
-    @user = User.find(params[:id])
-    current_user.unfollow(@user)
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
   end
 end
