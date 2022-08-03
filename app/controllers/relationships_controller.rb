697d8fb21ad80c6bef5bf5bf2e4a39ec24dac6d9
rilleague <aarima2626@gmail.com>
2022-08-03 23:34:46 +0900

    create,destoryアクション其々に、@userと@relationshipを定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,20 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_relationship
+
   # フォローする
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @relationship = current_user.follow(params[:user_id])
   end
 
   # フォロー解除する
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @relationship = current_user.unfollow(params[:user_id])
+  end
+
+  private
+
+  def set_relationship
+    @user = User.find(params[:user_id])
   end
 end
