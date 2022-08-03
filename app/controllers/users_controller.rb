3f04eab3257559dd16f024c9ec7a4e03424a6425
rilleague <aarima2626@gmail.com>
2022-08-03 23:35:09 +0900

    showアクション内に、@relationshipを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @post2 = Post.where(category_id: "3", user_id: @user.id).order("created_at DESC")
     @following_users = @user.following_user
     @follower_users = @user.follower_user
+    @relationship = Relationship.find_by(follower_id: current_user.id, followed_id: @user.id)
   end
 
   def edit
@@ -28,12 +29,14 @@ class UsersController < ApplicationController
   # 自分がフォローしているユーザーを表示するメソッドを定義
   def follows
     user = User.find(params[:id])
+    # user.rbでアソシエーションの記述following_userにより、フォローしているユーザー情報を持ってこれる
     @users = user.following_user.order("created_at DESC")
   end
 
   # 自分をフォローしているユーザーを表示するメソッドを定義
   def followers
     user = User.find(params[:id])
+    # user.rbでアソシエーションの記述follower_userにより、フォローされているユーザー情報を持ってこれる
     @users = user.follower_user.order("created_at DESC")
   end
 
