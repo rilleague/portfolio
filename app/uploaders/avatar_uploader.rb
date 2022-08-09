da70638416db40c1aa84865581c084946484136b
rilleague <aarima2626@gmail.com>
2022-08-09 18:31:56 +0900

    carrierwaveの画像サイズ設定を変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,7 +7,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # process resize_to_fit: [150, 150]
+  process resize_to_fit: [150, 150]
 
   def extension_allowlist
     %w(jpg jpeg gif png)
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_fill: [400, 300, 'Center'] 
+  process resize_to_fill: [400, 400, 'Center'] 
 
   # Choose what kind of storage to use for this uploader:
   storage :file
