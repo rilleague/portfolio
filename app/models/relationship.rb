aeb0190808a4f3b212dd3c23bd095adf8acf83d9
rilleague <aarima2626@gmail.com>
2022-08-18 19:25:59 +0900

    relationshipモデルの単体テスト

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,7 @@ class Relationship < ApplicationRecord
   # Followerモデルの参照が来たら、Userモデルを参照するように指定
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validates :follower_id, presence: true, uniqueness: { scope: :followed_id }
+  validates :followed_id, presence: true
 end
