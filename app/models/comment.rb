d9fbbd0faac2a084eed73166dc8adaab980b34ea
rilleague <aarima2626@gmail.com>
2022-08-18 19:26:40 +0900

    commentモデルの単体テスト

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :content, presence: true
+  # コメントは200文字以内
+  validates :content, presence: true, length: { maximum: 200 }
 end
