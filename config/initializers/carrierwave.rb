5e992218e3a506b14d9b958b3453927dbcd34c96
rilleague <aarima2626@gmail.com>
2022-09-02 16:01:43 +0900

    S3の実装編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,13 +10,13 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'polishine20220901'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/polishine20220901'
+    config.fog_public = false
     # iam_profile
     config.fog_credentials = {
       provider: 'AWS',
       # 環境変数で管理する場合
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1' #東京リージョン
       path_style: true
     }
