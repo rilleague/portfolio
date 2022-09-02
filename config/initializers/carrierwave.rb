72eabe20b4a46465caf11667d484c76fe69322f3
rilleague <aarima2626@gmail.com>
2022-09-02 16:39:58 +0900

    S3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,8 +16,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       # 環境変数で管理する場合
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1' #東京リージョン
       # path_style: true
     }
